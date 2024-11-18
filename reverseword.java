2d430df703f130f28c8543c2e9b7c7d768cc3589
Reena S <160510029+Reena-senthilkumar@users.noreply.github.com>
2024-11-18 21:53:23 +0530

    Add files via upload

---
+++ reverseword.java
@@ -0,0 +1,8 @@
+public class reverseword {
+	public static void main(String[] args) {
+	    String S="APPLE";
+	    StringBuilder sc=new StringBuilder(S);
+		System.out.println(sc.reverse());
+	}
+}
+
