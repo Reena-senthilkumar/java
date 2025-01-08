dc19ef68f2ffc87f07eae486d5c1f8c056c187b3
Reena S <160510029+Reena-senthilkumar@users.noreply.github.com>
2025-01-08 19:09:21 +0530

    Add files via upload

---
+++ Arithmeticprogress.java
@@ -0,0 +1,12 @@
+public class Arithmeticprogress 
+{
+    public static void main(String[] args) {
+        int A=11;
+        int B=22;
+        int C=33;
+        int N=70;
+        int different=B-A;
+        int nth=A+(N-1)*different;
+        System.err.println(nth);
+    }  
+}
