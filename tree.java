6703c3c8851865a87f44abf04b06aec86f9d346d
Reena S <160510029+Reena-senthilkumar@users.noreply.github.com>
2025-05-10 21:19:47 +0530

    Add files via upload

---
+++ tree.java
@@ -0,0 +1,16 @@
+class Node{
+    int data;
+    Node left;
+    Node right;
+
+public Node(int data){
+        this.data=data;
+        left=null;
+        right=null;
+    }
+    class BinaryTree{
+        Node root;
+        public  BinaryTree(){
+            root=null;
+        }
+    }}
