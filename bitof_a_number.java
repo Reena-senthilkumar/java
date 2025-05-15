d4bb2cf014271dec3cda406cefdaf97a5da5d903
Reena S <160510029+Reena-senthilkumar@users.noreply.github.com>
2025-05-15 21:18:23 +0530

    Add files via upload

---
+++ bitof_a_number.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class bitof_a_number {
+{
+    public static void binary(int n)
+   {
+       int bit=0;
+	    while(n!=0)
+	    {
+	        bit+=n%2;
+	         n=n/2;
+	        
+        }System.out.println(bit);
+    }
+}
+	public static void main(String[] args)
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+        binary(n);
+	}
+}
+
+
